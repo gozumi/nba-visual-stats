706f99384c8952750975f17021f3fdeb0b398df8
Ian Anderson <ian@gozumi.com>
2018-05-09 13:53:54 +0100

    refator(rxjs 6): Continue process to remove rxjs-compat module.

--- src/client/app/state/epics/index.ts
+++ src/client/app/state/epics/index.ts
@@ -9,7 +9,7 @@ import { filter, map } from 'rxjs/operators'
 import { GET_PLAYER_STATS, REQUEST_NEW_PLAYER_STATS_AGGREGATION } from '../action-types'
 import { getPlayerStatsFromWebWorker, requestAggregationChangeFromWebWorker } from './web-worker-instances/player-stats'
 
-const rootEpic: Epic<IAction, Dispatch<IAction>> = combineEpics(
+const rootEpic = combineEpics(
   getPlayerStats,
   changePlayerStatsAggregation
 )
