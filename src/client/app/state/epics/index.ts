0d42a75fb34a4902848274d6322bd3c5f7559265
Ian Anderson <ian@gozumi.com>
2018-05-09 13:26:40 +0100

    refator(rxjs 6): Continue process to remove rxjs-compat module.

--- src/client/app/state/epics/index.ts
+++ src/client/app/state/epics/index.ts
@@ -4,6 +4,7 @@ import 'rxjs/add/operator/map'
 import { IAction, IState } from 'client/app/state/store'
 import { Dispatch, Store } from 'redux'
 import { ActionsObservable, combineEpics, Epic } from 'redux-observable'
+import { filter, map } from 'rxjs/operators'
 
 import { GET_PLAYER_STATS, REQUEST_NEW_PLAYER_STATS_AGGREGATION } from '../action-types'
 import { getPlayerStatsFromWebWorker, requestAggregationChangeFromWebWorker } from './web-worker-instances/player-stats'
@@ -23,9 +24,11 @@ function getPlayerStats (action$: ActionsObservable<IAction>) {
 
 function changePlayerStatsAggregation (action$: ActionsObservable<IAction>, store: Store<IState>) {
   return action$
-    .filter((action) => action.type === REQUEST_NEW_PLAYER_STATS_AGGREGATION)
-    .map((action) => {
-      const { list } = store.getState().playerStats
-      return requestAggregationChangeFromWebWorker(action.payload, list)
-    })
+    .pipe(
+      filter((action) => action.type === REQUEST_NEW_PLAYER_STATS_AGGREGATION),
+      map((action) => {
+        const { list } = store.getState().playerStats
+        return requestAggregationChangeFromWebWorker(action.payload, list)
+      })
+    )
 }
